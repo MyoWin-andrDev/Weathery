9e9d245b83568f34c36ea53319835e1932905626
Phyo Hein ( Ezekiel ) <mwrcps11@gmail.com>
2024-12-14 14:12:49 +0700

    Initial Commit

---
+++ app/src/main/java/it/ezzie/weathery/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package it.ezzie.weathery.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val DarkNavyBlue = Color(0xFF1A2332)
+val DarkerNavyBlue = Color(0xFF161C2A)
+val Yellow = Color(0xFFFFFF00)
+val GreyBorder = Color(0xFF808080)
+val Silver = Color(0xFFC0C0C0)
